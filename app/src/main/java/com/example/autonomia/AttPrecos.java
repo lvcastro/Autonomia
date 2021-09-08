398d2f1e77bad9993b0c702944ae9f01f9898f2b
Lucas Vizzotto de Castro <lucasvizzottodecastro@gmail.com>
2021-09-08 11:09:44 -0300

    - Adicionado botão na tela de atualização dos preços que redireciona para a tela de avaliação do posto

--- app/src/main/java/com/example/autonomia/AttPrecos.java
+++ app/src/main/java/com/example/autonomia/AttPrecos.java
@@ -8,9 +8,24 @@ import android.view.View;
 import android.widget.Button;
 
 public class AttPrecos extends AppCompatActivity {
+
+    private Button btnAvalPosto;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_att_precos);
+
+        btnAvalPosto = (Button) findViewById(R.id.btnAvalPosto);
+
+        btnAvalPosto.setOnClickListener( new AttPrecos.EscutadorBotaoAvalPosto());
+    }
+
+    private class EscutadorBotaoAvalPosto implements View.OnClickListener{
+        @Override
+        public void onClick(View view) {
+            Intent i = new Intent( getApplicationContext(), AvalPosto.class);
+            startActivity(i);
+        }
     }
 }
