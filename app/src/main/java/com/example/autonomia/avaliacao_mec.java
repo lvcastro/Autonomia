9d88b3c8ebba349bdcc5364c0c1f02cbbbac1201
froesLucas <84995236+froesLucas@users.noreply.github.com>
2021-09-08 12:02:43 -0300

    Add files via upload

---
+++ app/src/main/java/com/example/autonomia/avaliacao_mec.java
@@ -0,0 +1,14 @@
+package com.example.pji;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class avaliacao_mec extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_avaliacao_mec);
+    }
+}
