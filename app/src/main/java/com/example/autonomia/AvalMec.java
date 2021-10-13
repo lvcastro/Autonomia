424d1e0ab6548464c2546f25705eff5ad149c4a3
Lucas Vizzotto de Castro <lucasvizzottodecastro@gmail.com>
2021-10-13 11:13:22 -0300

    - Correção do erro no "gradle.properties"

---
+++ app/src/main/java/com/example/autonomia/AvalMec.java
@@ -0,0 +1,14 @@
+package com.example.autonomia;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class AvalMec extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_avaliacao_mec);
+    }
+}
