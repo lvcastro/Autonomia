055e3ca86b71f33d1e0b7b7f144f8a2b918df953
Lucas Vizzotto de Castro <lucasvizzottodecastro@gmail.com>
2021-10-20 11:58:08 -0300

    - Correções finais na interface gráfica; - Adição dos botões para navegar entre telas

--- app/src/main/java/com/example/autonomia/CadPosto.java
+++ app/src/main/java/com/example/autonomia/CadPosto.java
@@ -10,6 +10,10 @@ import android.widget.Button;
 public class CadPosto extends AppCompatActivity {
 
     private Button btnAttPrecos;
+    private Button btnAvalPosto;
+    private Button btnAvalMec;
+    private Button btnCadMec;
+    private Button btnCadUsuario;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -17,14 +21,54 @@ public class CadPosto extends AppCompatActivity {
         setContentView(R.layout.activity_cad_posto);
 
         btnAttPrecos = (Button) findViewById(R.id.btnAttPrecos);
+        btnAvalPosto = (Button) findViewById(R.id.btnAvalPosto);
+        btnAvalMec = (Button) findViewById(R.id.btnAvalMec);
+        btnCadMec = (Button) findViewById(R.id.btnCadMec);
+        btnCadUsuario = (Button) findViewById(R.id.btnCadUsuario);
 
         btnAttPrecos.setOnClickListener( new EscutadorBotaoAttPrecos());
+        btnAvalPosto.setOnClickListener( new EscutadorBotaoAvalPosto());
+        btnAvalMec.setOnClickListener( new EscutadorBotaoAvalMec());
+        btnCadMec.setOnClickListener( new EscutadorBotaoCadMec());
+        btnCadUsuario.setOnClickListener( new EscutadorBotaoCadUsuario());
     }
 
     private class EscutadorBotaoAttPrecos implements View.OnClickListener{
         @Override
         public void onClick(View view) {
             Intent i = new Intent( getApplicationContext(), AttPrecos.class);
+            startActivity(i);
+        }
+    }
+
+    private class EscutadorBotaoAvalPosto implements View.OnClickListener{
+        @Override
+        public void onClick(View view) {
+            Intent i = new Intent( getApplicationContext(), AvalPosto.class);
+            startActivity(i);
+        }
+    }
+
+    private class EscutadorBotaoAvalMec implements View.OnClickListener{
+        @Override
+        public void onClick(View view) {
+            Intent i = new Intent( getApplicationContext(), AvalMec.class);
+            startActivity(i);
+        }
+    }
+
+    private class EscutadorBotaoCadMec implements View.OnClickListener{
+        @Override
+        public void onClick(View view) {
+            Intent i = new Intent( getApplicationContext(), CadMec.class);
+            startActivity(i);
+        }
+    }
+
+    private class EscutadorBotaoCadUsuario implements View.OnClickListener{
+        @Override
+        public void onClick(View view) {
+            Intent i = new Intent( getApplicationContext(), CadUsuario.class);
             startActivity(i);
         }
     }
